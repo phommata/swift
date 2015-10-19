61a5950456e2fe59c2a096601dae7d3f9f842a55
Andrew Phommathep <andrew@starclubltd.com>
2015-10-19 12:21:05 -0700

    4 Setting the Attributes for the Second Text Field

--- Control Fun/Control Fun/ViewController.swift
+++ Control Fun/Control Fun/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
 }
 
