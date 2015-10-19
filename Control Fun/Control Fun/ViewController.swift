2d1481163969ca43f3a14f71a8931e1b7d326ebf
Andrew Phommathep <andrew@starclubltd.com>
2015-10-19 15:29:42 -0700

    4 Implementing the Segmented Control Action, Action Sheet and Alert
    Implementing the Action Sheet and Alert: Showing an Action Sheet

--- Control Fun/Control Fun/ViewController.swift
+++ Control Fun/Control Fun/ViewController.swift
@@ -50,9 +50,48 @@ class ViewController: UIViewController {
     }
     
     @IBAction func toggleControls(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            leftSwitch.hidden = false
+            rightSwitch.hidden = false
+            doSomethingButton.hidden = true
+        } else {
+            leftSwitch.hidden = true
+            rightSwitch.hidden = true
+            doSomethingButton.hidden = false
+        }
     }
     
     @IBAction func buttonPressed(sender: UIButton) {
+        let controller = UIAlertController(title: "Are You Sure?",
+                        message:nil, preferredStyle: .ActionSheet)
+            
+        let yesAction = UIAlertAction(title: "Yes, I'm sure!",
+                        style: .Destructive, handler: { action in
+            let msg = self.nameField.text.isEmpty
+                    ? "You can breathe easy, everything went OK."
+                    : "You can breathe easy, \(self.nameField.text),"
+                        + " everything went OK."
+            let controller2 = UIAlertController(
+                            title:"Something Was Done",
+                            message: msg, preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "Phew!",
+                        style: .Cancel, handler: nil)
+                        controller2.addAction(cancelAction)
+            self.presentViewController(controller2, animated: true,
+                                        completion: nil)
+        })
+        let noAction = UIAlertAction(title: "No way!",
+                                    style: .Cancel, handler: nil)
+            
+        controller.addAction(yesAction)
+        controller.addAction(noAction)
+            
+        if let ppc = controller.popoverPresentationController {
+            ppc.sourceView = sender
+            ppc.sourceRect = sender.bounds
+        }
+            
+        presentViewController(controller, animated: true, completion: nil)
     }
 }
 
