043abf51c31e1be5adda874b26fbefb1f913e099
Andrew Phommathep <andrew@starclubltd.com>
2015-10-21 14:46:43 -0700

    6 Building View Switcher
    Renaming the View Controller
    Adding the Content View Controllers
    Modifying SwitchingViewController.swift
    Building a View with a Toolbar

---
+++ 6 View Switcher/6 View Switcher/SwitchingViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SwitchingViewController.swift
+//  6 View Switcher
+//
+//  Created by Andrew Phommathep on 10/21/15.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class SwitchingViewController: UIViewController {
+    
+    private var blueViewController: BlueViewController!
+    private var yellowViewController: YellowViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func switchViews(sender: UIBarButtonItem) {
+    }
+}
