36f42a5d92b297a23cf0f491388d1949c87b43b3
Andrew Phommathep <andrew@starclubltd.com>
2015-10-29 16:42:19 -0700

    8 Implementing a Simple Table
    Designing the View

---
+++ 8 Simple Table/8 Simple Table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  8 Simple Table
+//
+//  Created by Andrew Phommathep on 10/29/15.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 8 Simple Table/8 Simple TableTests/__Simple_TableTests.swift
@@ -0,0 +1,36 @@
+//
+//  __Simple_TableTests.swift
+//  8 Simple TableTests
+//
+//  Created by Andrew Phommathep on 10/29/15.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class __Simple_TableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
