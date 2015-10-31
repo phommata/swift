09c9cad0b29b3ddbd43b646901d772dcdc4668ae
Andrew Phommathep <andrew@starclubltd.com>
2015-10-31 11:15:08 -0700

    8 Writing the Controller

--- 8 Simple Table/8 Simple Table/ViewController.swift
+++ 8 Simple Table/8 Simple Table/ViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    private let dwarves = [
+        "Sleepy", "Sneezy", "Bashful", "Happy",
+        "Doc", "Grumpy", "Dopey",
+        "Thorin", "Dorin", "Nori", "Ori",
+        "Balin", "Dwalin", "Fili", "Kili",
+        "Oin", "Gloin", "Bifur", "Bofur",
+        "Bombur"
+    ]
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView,
+            numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(
+        simpleTableIdentifier) as? UITableViewCell
+        if (cell == nil) {
+            cell = UITableViewCell(
+            style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
+        }
+        cell!.textLabel?.text = dwarves[indexPath.row]
+        return cell!
+    }
 }
 
