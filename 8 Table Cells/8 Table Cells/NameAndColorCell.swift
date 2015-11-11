f43ddca197a5b2be5cd4c6afd706f1c0e2956c71
Andrew Phommathep <13destinies@gmail.com>
2015-11-11 09:38:42 -0800

    8 Implementing the Controller’s Code

--- 8 Table Cells/8 Table Cells/NameAndColorCell.swift
+++ 8 Table Cells/8 Table Cells/NameAndColorCell.swift
@@ -12,30 +12,21 @@ class NameAndColorCell: UITableViewCell {
 
     var name: String = "" {
         didSet {
-        if (name != oldValue) {
-        nameLabel.text = name
-        } }
+            if (name != oldValue) {
+                nameLabel.text = name
+            }
+        }
     }
     var color: String = "" {
         didSet {
-        if (color != oldValue) {
-        colorLabel.text = color
+            if (color != oldValue) {
+                colorLabel.text = color
+            }
         }
-        } }
+    }
     var nameLabel: UILabel!
     var colorLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
             
@@ -63,6 +54,18 @@ class NameAndColorCell: UITableViewCell {
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+//        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
     }
 }
